de98b201e9192773a44b1bca79d764f3a7de0d4d
ScientistMonkey <35115220+ScientistMonkey@users.noreply.github.com>
2019-09-12 14:12:02 +0200

    Linked LED.py with MAIN.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import pygame
 from mutagen.id3 import ID3
 from tkinter import *
 
+from sense_hat import SenseHat
+from time import sleep
+import time
+
+sense = SenseHat()
+
 root = Tk()
 root.minsize(300,300)
 
@@ -16,6 +22,12 @@ v = StringVar()
 songlabel = Label(root,textvariable=v,width=35)
 
 index = 0
+
+sense.clear()
+volume = 0
+paused = False
+print("test")
+teller = 0
 
 def directorychooser():
 
@@ -35,7 +47,7 @@ def directorychooser():
 
     pygame.mixer.init()
     pygame.mixer.music.load(listofsongs[0])
-    #pygame.mixer.music.play()
+    # pygame.mixer.music.play()
 
 directorychooser()
 
@@ -45,26 +57,198 @@ def updatelabel():
     v.set(realnames[index])
     #return songname
 
+def toggle_pause(event):
+    global index
+    index += 1
+    pygame.mixer.music.load(listofsongs[index])
+    pygame.mixer.music.play()
+    global paused
+    global teller
+    if teller == 0:
+        
+        print(paused)
+        paused = not paused
+        if paused and event.action == 'pressed' and event.direction == 'middle':
+            pausesong(event)
+        elif event.action == 'pressed' and event.direction == 'middle':
+            play(event)
+        teller = teller + 1
+    else:
+        teller = 0
+
+
+def play(event):
+    global index
+    index
+    pygame.mixer.music.play()
+    sense.clear()
+    sense.set_pixel(1, 1, (255, 255, 255))
+    sense.set_pixel(2, 2, (255, 255, 255))
+    sense.set_pixel(1, 2, (255, 255, 255))
+    sense.set_pixel(1, 3, (255, 255, 255))
+    sense.set_pixel(1, 4, (255, 255, 255))
+    sense.set_pixel(1, 5, (255, 255, 255))
+    sense.set_pixel(1, 6, (255, 255, 255))
+    sense.set_pixel(2, 1, (255, 255, 255))
+    sense.set_pixel(2, 3, (255, 255, 255))
+    sense.set_pixel(2, 4, (255, 255, 255))
+    sense.set_pixel(2, 5, (255, 255, 255))
+    sense.set_pixel(2, 6, (255, 255, 255))
+    sense.set_pixel(5, 1, (255, 255, 255))
+    sense.set_pixel(5, 2, (255, 255, 255))
+    sense.set_pixel(5, 3, (255, 255, 255))
+    sense.set_pixel(5, 4, (255, 255, 255))
+    sense.set_pixel(5, 5, (255, 255, 255))
+    sense.set_pixel(5, 6, (255, 255, 255))
+    sense.set_pixel(6, 1, (255, 255, 255))
+    sense.set_pixel(6, 2, (255, 255, 255))
+    sense.set_pixel(6, 3, (255, 255, 255))
+    sense.set_pixel(6, 4, (255, 255, 255))
+    sense.set_pixel(6, 5, (255, 255, 255))
+    sense.set_pixel(6, 6, (255, 255, 255))
+    sleep(2)
+    sense.clear()
+
 
 def nextsong(event):
     global index
     index += 1
     pygame.mixer.music.load(listofsongs[index])
     pygame.mixer.music.play()
-    updatelabel()
+    global teller
+    if teller == 0:
+        sense.clear()
+        sense.set_pixel(0, 1, (255, 255, 255))
+        sense.set_pixel(0, 2, (255, 255, 255))
+        sense.set_pixel(0, 3, (255, 255, 255))
+        sense.set_pixel(0, 4, (255, 255, 255))
+        sense.set_pixel(0, 5, (255, 255, 255))
+        sense.set_pixel(0, 6, (255, 255, 255))
+        sense.set_pixel(1, 2, (255, 255, 255))
+        sense.set_pixel(1, 3, (255, 255, 255))
+        sense.set_pixel(1, 4, (255, 255, 255))
+        sense.set_pixel(1, 5, (255, 255, 255))
+        sense.set_pixel(2, 3, (255, 255, 255))
+        sense.set_pixel(2, 4, (255, 255, 255))
+        sense.set_pixel(5, 1, (255, 255, 255))
+        sense.set_pixel(5, 2, (255, 255, 255))
+        sense.set_pixel(5, 3, (255, 255, 255))
+        sense.set_pixel(5, 4, (255, 255, 255))
+        sense.set_pixel(5, 5, (255, 255, 255))
+        sense.set_pixel(5, 6, (255, 255, 255))
+        sense.set_pixel(6, 2, (255, 255, 255))
+        sense.set_pixel(6, 3, (255, 255, 255))
+        sense.set_pixel(6, 4, (255, 255, 255))
+        sense.set_pixel(6, 5, (255, 255, 255))
+        sense.set_pixel(7, 3, (255, 255, 255))
+        sense.set_pixel(7, 4, (255, 255, 255))
+        teller = teller + 1
+        sleep(2)
+        sense.clear()
+        index -= 1 
+        # ^^ VERDOMME HET WERKT
+        updatelabel()
+        # next
+    else:
+        teller = 0
 
 def prevsong(event):
     global index
     index -= 1
     pygame.mixer.music.load(listofsongs[index])
     pygame.mixer.music.play()
-    updatelabel()
+    global teller
+    if teller == 0:
+        sense.clear()
+        sense.set_pixel(0, 3, (255, 255, 255))
+        sense.set_pixel(0, 4, (255, 255, 255))
+        sense.set_pixel(1, 2, (255, 255, 255))
+        sense.set_pixel(1, 3, (255, 255, 255))
+        sense.set_pixel(1, 4, (255, 255, 255))
+        sense.set_pixel(1, 5, (255, 255, 255))
+        sense.set_pixel(2, 1, (255, 255, 255))
+        sense.set_pixel(2, 2, (255, 255, 255))
+        sense.set_pixel(2, 3, (255, 255, 255))
+        sense.set_pixel(2, 4, (255, 255, 255))
+        sense.set_pixel(2, 5, (255, 255, 255))
+        sense.set_pixel(2, 6, (255, 255, 255))
+        sense.set_pixel(5, 3, (255, 255, 255))
+        sense.set_pixel(5, 4, (255, 255, 255))
+        sense.set_pixel(6, 2, (255, 255, 255))
+        sense.set_pixel(6, 3, (255, 255, 255))
+        sense.set_pixel(6, 4, (255, 255, 255))
+        sense.set_pixel(6, 5, (255, 255, 255))
+        sense.set_pixel(7, 1, (255, 255, 255))
+        sense.set_pixel(7, 2, (255, 255, 255))
+        sense.set_pixel(7, 3, (255, 255, 255))
+        sense.set_pixel(7, 4, (255, 255, 255))
+        sense.set_pixel(7, 5, (255, 255, 255))
+        sense.set_pixel(7, 6, (255, 255, 255))
+        teller = teller + 1
+        sleep(2)
+        index += 1
+        sense.clear()
+        updatelabel()
+    else:
+        teller = 0
 
 
-def stopsong(event):
+def pausesong(event):
     pygame.mixer.music.stop()
     v.set("")
-    #return songname
+    sense.clear()
+    sense.set_pixel(1, 0, (255, 255, 255))
+    sense.set_pixel(2, 0, (255, 255, 255))
+    sense.set_pixel(1, 1, (255, 255, 255))
+    sense.set_pixel(1, 2, (255, 255, 255))
+    sense.set_pixel(1, 3, (255, 255, 255))
+    sense.set_pixel(1, 4, (255, 255, 255))
+    sense.set_pixel(1, 5, (255, 255, 255))
+    sense.set_pixel(1, 6, (255, 255, 255))
+    sense.set_pixel(1, 7, (255, 255, 255))
+    sense.set_pixel(2, 1, (255, 255, 255))
+    sense.set_pixel(2, 2, (255, 255, 255))
+    sense.set_pixel(2, 3, (255, 255, 255))
+    sense.set_pixel(2, 4, (255, 255, 255))
+    sense.set_pixel(2, 5, (255, 255, 255))
+    sense.set_pixel(2, 6, (255, 255, 255))
+    sense.set_pixel(2, 7, (255, 255, 255))
+    sense.set_pixel(3, 1, (255, 255, 255))
+    sense.set_pixel(3, 2, (255, 255, 255))
+    sense.set_pixel(3, 3, (255, 255, 255))
+    sense.set_pixel(3, 4, (255, 255, 255))
+    sense.set_pixel(3, 5, (255, 255, 255))
+    sense.set_pixel(3, 6, (255, 255, 255))
+    sense.set_pixel(4, 1, (255, 255, 255))
+    sense.set_pixel(4, 2, (255, 255, 255))
+    sense.set_pixel(4, 3, (255, 255, 255))
+    sense.set_pixel(4, 4, (255, 255, 255))
+    sense.set_pixel(4, 5, (255, 255, 255))
+    sense.set_pixel(4, 6, (255, 255, 255))
+    sense.set_pixel(5, 2, (255, 255, 255))
+    sense.set_pixel(5, 3, (255, 255, 255))
+    sense.set_pixel(5, 4, (255, 255, 255))
+    sense.set_pixel(5, 5, (255, 255, 255))
+    sense.set_pixel(6, 3, (255, 255, 255))
+    sense.set_pixel(6, 4, (255, 255, 255))
+    # return songname
+
+def volume_control():
+    sense.set_pixel(volume + 1, 3, (255, 255, 255))
+    sense.set_pixel(volume + 1, 4, (255, 255, 255))
+
+def volume_up(event):
+    global volume
+    volume_control()
+    if event.action == 'pressed':
+        volume += 1
+
+def volume_down(event):
+    global volume
+    if event.action == 'pressed':
+        volume -= 1
+        sense.set_pixel(6, 3, 0, 0, 0)
+        sense.set_pixel(6, 4, 0, 0, 0)
 
 
 label = Label(root,text='Music Player')
@@ -73,15 +257,20 @@ label.pack()
 listbox = Listbox(root)
 listbox.pack()
 
-#listofsongs.reverse()
-realnames.reverse()
+listofsongs.reverse()
+# realnames.reverse()
 
 for items in realnames:
     listbox.insert(0,items)
 
 realnames.reverse()
-#listofsongs.reverse()
+# listofsongs.reverse()
 
+sense.stick.direction_middle = toggle_pause
+sense.stick.direction_down = volume_down
+sense.stick.direction_up = volume_up
+sense.stick.direction_right = nextsong
+sense.stick.direction_left = prevsong
 
 nextbutton = Button(root,text = 'Next Song')
 nextbutton.pack()
@@ -94,7 +283,7 @@ stopbutton.pack()
 
 nextbutton.bind("<Button-1>",nextsong)
 previousbutton.bind("<Button-1>",prevsong)
-stopbutton.bind("<Button-1>",stopsong)
+stopbutton.bind("<Button-1>",pausesong)
 
 songlabel.pack()
 
